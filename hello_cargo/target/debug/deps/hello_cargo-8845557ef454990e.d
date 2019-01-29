66546a0a364b4ff2dd911d8a37769be9d348108c
mgmilton <matt.milton@gmail.com>
2019-01-28 16:37:26 -0800

    first commit

---
+++ hello_cargo/target/debug/deps/hello_cargo-8845557ef454990e.d
@@ -0,0 +1,5 @@
+/Users/mattm/enbala/code_alongs/ferric_oxide/hello_cargo/target/debug/deps/hello_cargo-8845557ef454990e: src/main.rs
+
+/Users/mattm/enbala/code_alongs/ferric_oxide/hello_cargo/target/debug/deps/hello_cargo-8845557ef454990e.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/mattm/enbala/code_alongs/ferric_oxide/hello_cargo/target/debug/hello_cargo.dSYM: /Users/mattm/enbala/code_alongs/ferric_oxide/hello_cargo/src/main.rs
